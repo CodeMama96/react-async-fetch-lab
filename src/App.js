34e84420ecb420768078b05515e387bf64ec13d3
Alicia Weenum <weenum.alicia@gmail.com>
2021-03-04 15:57:03 -0500

    commit tree afcaa851dcd4ab8edb3ae48202c2f584d9ab6b86

--- src/App.js
+++ src/App.js
@@ -1,1 +1,36 @@
 // create your App component here
+import React, { Component } from 'react';
+
+//no need to modify anything in this component
+class App extends Component {
+
+  state = {
+    peopleInSpace: []
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        peopleInSpace: data.people
+      })
+    })
+  }
+
+  render() {
+ 
+    return (
+      <div>
+          {this.state.peopleInSpace.map(person => person.name)}
+      </div>
+    );
+  }
+
+  // returns array of components written in JSX, mapped from this.state.timerIDs
+  renderTimers = () => this.state.timerIDs.map(({id, updateInterval}) => <Timer key={id} id={id} removeTimer={this.removeTimer} updateInterval={updateInterval}/>)
+
+
+}
+
+export default App;
